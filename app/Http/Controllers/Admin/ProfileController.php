df09ead02fbaccc20d7d8b0aae3d2e29b8d3060f
[shotauchi] <[tianneixiang84@gmail.com]>
2024-09-28 19:28:15 +0000

    コミット12

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+// 以下の1行を追記することで、News Modelが扱えるようになる
+use App\Models\Profile;
+
 class ProfileController extends Controller
 {
     public function add()
@@ -12,9 +15,32 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
     
-    public function create()
+    public function create(Request $request)
     {
-        return redrect('admin/profile/create');
+        // Validationを行う
+        $this->validate($request, Profile::$rules);
+
+        $profile = new Profile;
+        $form = $request->all();
+
+        // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+        if (isset($form['image'])) {
+            $path = $request->file('image')->store('public/image');
+            $profile->image_path = basename($path);
+        } else {
+            $profile->image_path = null;
+        }
+
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // フォームから送信されてきたimageを削除する
+        unset($form['image']);
+
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();
+        
+        return redirect('admin/profile/create');
     }
     
     public function edit()
